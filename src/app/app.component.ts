12956de8f6a4a5de02e8d37eaee8805638f367ed
Pedro Poveda <>
2020-02-24 14:33:14 -0500

    Initial Commit: Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {}
+export class AppComponent {
+  submitted = false;
+  gender = null;
+  weight = null;
+  height = null;
+  age = null;
+  activityVariable = null;
+  macros = {
+    lowCarb: { protein: 0, carbs: 0, fats: 0 },
+    highCarb: { protein: 0, carbs: 0, fats: 0 }
+  };
 
+  public calculateMacros(): void {
+    this.submitted = true;
+    if (this.gender == null) {
+      alert("Gender not Selected!");
+    }
+
+    if (this.gender == "male") {
+      let bmr = 10 * this.weight + 6.25 * this.height - 5 * this.age + 5;
+      let dcr = bmr * this.activityVariable;
+      let lowCarbDeficit = dcr * 0.75;
+      let highCarbDeficit = dcr * 0.9;
+
+      this.macros.lowCarb.protein = (lowCarbDeficit * 0.4) / 4;
+      this.macros.lowCarb.carbs = (lowCarbDeficit * 0.35) / 4;
+      this.macros.lowCarb.fats = (lowCarbDeficit * 0.25) / 9;
+
+      this.macros.highCarb.protein = (highCarbDeficit * 0.3) / 4;
+      this.macros.highCarb.carbs = (highCarbDeficit * 0.5) / 4;
+      this.macros.highCarb.fats = (highCarbDeficit * 0.2) / 9;
+    } else {
+      let bmr = 10 * this.weight + 6.25 * this.height - 5 * this.age - 161;
+      let dcr = bmr * this.activityVariable;
+      let lowCarbDeficit = dcr * 0.8;
+      let highCarbDeficit = dcr * 0.9;
+
+      this.macros.lowCarb.protein = (lowCarbDeficit * 0.35) / 4;
+      this.macros.lowCarb.carbs = (lowCarbDeficit * 0.4) / 4;
+      this.macros.lowCarb.fats = (lowCarbDeficit * 0.25) / 9;
+
+      this.macros.highCarb.protein = (highCarbDeficit * 0.3) / 4;
+      this.macros.highCarb.carbs = (highCarbDeficit * 0.5) / 4;
+      this.macros.highCarb.fats = (highCarbDeficit * 0.2) / 9;
+    }
+  }
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
